b288a826a24709f38d9f15f11b5b6356635b33ec
Rz Rasel <emailrzrasel@gmail.com>
2021-08-02 21:41:03 +0600

    Rz Android Java SDK - Add rzandjavagit-core libs

--- rzandroid-core/src/main/java/com/rzandroidgit/core/AndCoreTest.java
+++
@@ -1,7 +0,0 @@
-package com.rzandroidgit.core;
-
-public class AndCoreTest {
-    public static void onPrint(String argMessage) {
-        System.out.println("DEBUG_PRINT_FROM_CORE: " + argMessage);
-    }
-}
--- rzandroid/src/main/java/com/rzandroidgit/AndTest.java
+++
@@ -1,7 +0,0 @@
-package com.rzandroidgit;
-
-public class AndTest {
-    public static void onPrint(String argMessage) {
-        System.out.println("DEBUG_PRINT_FROM_AND: " + argMessage);
-    }
-}
