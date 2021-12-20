f6dbcff81b34643cf7829edcb0c8ae902e702415
Rz Rasel <emailrzrasel@gmail.com>
2021-12-20 17:07:31 +0600

    ProInternet class created → Monday December 20, 2021, 17:03:01

--- rzandjavagit-prointernet/src/main/java/com/rzandjavagit/prointernet/ProInternet.java
+++ rzandjavagit-prointernet/src/main/java/com/rzandjavagit/prointernet/ProInternet.java
@@ -18,10 +18,39 @@ public class ProInternet {
         }
         return instance;
     }
+
     public static boolean hasConnected(Context argContext) {
         return getInstance().isConnected(argContext);
     }
-    public boolean isConnected(Context argContext) {
+
+    public static boolean isConnected(Context argContext) {
+        int hasPermission = 0;
+        hasPermission = ContextCompat.checkSelfPermission((Activity) argContext, Manifest.permission.INTERNET);
+        if (hasPermission != PackageManager.PERMISSION_GRANTED) {
+            //LogWriter.Log("Check it", "Please set the permission INTERNET");
+            return false;
+        }
+        hasPermission = ContextCompat.checkSelfPermission((Activity) argContext, Manifest.permission.ACCESS_NETWORK_STATE);
+        if (hasPermission != PackageManager.PERMISSION_GRANTED) {
+            //LogWriter.Log("Check it", "Please set the permission ACCESS_NETWORK_STATE");
+            return false;
+        }
+        ConnectivityManager connectivityManager = (ConnectivityManager) argContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
+            if (networkInfos != null) {
+                for (int i = 0; i < networkInfos.length; i++) {
+                    if (networkInfos[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+
+        }
+        return false;
+    }
+
+    private boolean isConnectedOld(Context argContext) {
         //if (Build.VERSION.SDK_INT >= 23)
         int hasPermission = 0;
         hasPermission = ContextCompat.checkSelfPermission((Activity) argContext, Manifest.permission.INTERNET);
